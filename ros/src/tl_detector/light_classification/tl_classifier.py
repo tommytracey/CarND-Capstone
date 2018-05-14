db4c5bdec140c4aa111ea81c2ba5c1764ecad490
Saajan Shridhar <saajan@saajan-macbookpro3.roam.corp.google.com>
2018-05-13 18:21:44 -0700

    Unit tested (and failed) code

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,7 +1,6 @@
 from styx_msgs.msg import TrafficLight
 import rospy
 
-
 import tensorflow as tf
 import cv2
 import numpy as np
@@ -10,14 +9,16 @@ import glob
 
 class TLClassifier(object):
     def __init__(self, model_path, confidence_threshold):
+        
+        self.confidence_threshold = confidence_threshold
 
-        self.graph = tf.graph()
+        self.graph = tf.Graph()
 
         config = tf.ConfigProto()
-        config.gpu_options.allow_growth = true
+        config.gpu_options.allow_growth = True
 
         with self.graph.as_default():
-            graph_def = tf.graph_def
+            graph_def = tf.GraphDef()
 
             with tf.gfile.GFile(model_path, 'rb') as graph_file:
                 read_graph_file = graph_file.read()
@@ -25,14 +26,14 @@ class TLClassifier(object):
                 graph_def.ParseFromString(read_graph_file)
                 tf.import_graph_def(graph_def, name='')
 
-            self.sess = tf.Session(graph=self.detection_graph, config=config)
+            self.sess = tf.Session(graph=self.graph, config=config)
 
-            self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
-            self.num_detections =self.detection_graph.get_tensor_by_name('num_detections:0')
+            self.image_tensor = self.graph.get_tensor_by_name('image_tensor:0')
+            self.num_detections =self.graph.get_tensor_by_name('num_detections:0')
             # For each detection, it's corresponding bounding box, class and score:
-            # self.boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
-            self.classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
-            self.scores =self.detection_graph.get_tensor_by_name('detection_scores:0')
+            # self.boxes = self.graph.get_tensor_by_name('detection_boxes:0')
+            self.classes = self.graph.get_tensor_by_name('detection_classes:0')
+            self.scores =self.graph.get_tensor_by_name('detection_scores:0')
 
 
     def get_classification(self, image):
@@ -61,37 +62,56 @@ class TLClassifier(object):
             rospy.loginfo("[TL Detector] Classes: {0}".format(classes))
             rospy.loginfo("[TL Detector] Scores: {0}".format(scores))
 
-            class_predictions_above_threshold = []
+            
+#             ##############
+#             ## Strategy 1: pick the most frequent prediction (consider replacing 
+#             # with weighted frequency)
+#             #
+#             #
+#             class_predictions_above_threshold = []
+#             for detected_class, score in zip(classes, scores):
+#                 if score > self.confidence_threshold:
+#                     class_predictions_above_threshold.append(detected_class)
+
+#             if (len(class_predictions_above_threshold) == 0):
+#                 rospy.loginfo("[Detector] Predicted Class: Unknown")
+#                 return TrafficLight.UNKNOWN
+                
+#             class_predictions = Counter(class_predictions_above_threshold)
+#             most_frequent_class = class_predictions.most_common(1)[0][0]
+
+#             if most_frequent_class == 1:
+#                 rospy.loginfo("[Detector] Predicted Class: Red")
+#                 return TrafficLight.RED
+#             elif most_frequent_class == 2:
+#                 rospy.loginfo("[Detector] Predicted Class: Yellow")
+#                 return TrafficLight.YELLOW
+#             elif most_frequent_class == 3:
+#                 rospy.loginfo("[Detector] Predicted Class: Green")
+#                 return TrafficLight.GREEN
+#             #
+#             #
+#             ##############
+            
+            
+            ##############
+            ## Strategy 2: pick the first prediction above threshold
             for detected_class, score in zip(classes, scores):
-                if score > self.confidence_threshold:
-                    class_predictions_above_threshold.append(detected_class)
-
-            class_predictions = Counter(class_predictions_above_threshold)
-            most_frequent_class = class_predictions.most_common(1)[0][0]
-
-            if most_frequent_class == 1:
-                rospy.loginfo("[Detector] Predicted Class: Red")
-                return TrafficLight.RED
-            elif most_frequent_class == 2:
-                rospy.loginfo("[Detector] Predicted Class: Yellow")
-                return TrafficLight.YELLOW
-            elif most_frequent_class == 3:
-                rospy.loginfo("[Detector] Predicted Class: Green")
-                return TrafficLight.GREEN
+                if score > self.confidence_threshold:                    
+                    if detected_class == 1:
+                        rospy.loginfo("[Detector] Predicted Class: Red")
+                        return TrafficLight.RED
+                    elif detected_class == 2:
+                        rospy.loginfo("[Detector] Predicted Class: Yellow")
+                        return TrafficLight.YELLOW
+                    elif detected_class == 3:
+                        rospy.loginfo("[Detector] Predicted Class: Green")
+                        return TrafficLight.GREEN
+                    
+            #
+            #
+            ##############
+            
 
         rospy.loginfo("[Detector] Predicted Class: Unknown")
         return TrafficLight.UNKNOWN
-
-if __name__ == '__main__':
-    classifier =TLClassifier(modelpath= "../data/saved_models/model_real/frozen_inference_graph.pb", threshold=0.3)
-
-    images_path = "../data/sample_images/*.png"
-    for image_path, expected_label in zip(
-            sorted(glob.glob(images_path)),
-            [3, 3, 3, 1, 1, 1, 2, 2, 2]):
-
-        image = cv2.imread(image_path)
-        traffic_light = classifier.get_classification(image)
-
-        if traffic_light != expected_label:
-            raise ValueError("Wrong class for image {0}".format(image_path))
